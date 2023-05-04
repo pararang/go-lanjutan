d8401e0cf5e3296cabd80db80b938d25d788002a
Muhammad Ikhsan <pararang@gmail.com>
2023-05-04 22:40:01 +0800

    feat(BE4855066.go): add HomeworkBE4855066 function to count word occurrences in input string, with a limit of 5 occurrences per word

---
+++ testing/BE4855066.go
@@ -0,0 +1,26 @@
+package mytesting
+
+// import "strings"
+
+// func HomeworkBE4855066(input string) map[string]int {
+// 	if input == "" {
+// 		return nil
+// 	}
+
+// 	if len(input) > 100 {
+// 		return map[string]int{
+// 			"input": 1,
+// 		}
+// 	}
+
+//     words := strings.Fields(input)
+//     wordCount := make(map[string]int)
+//     for _, word := range words {
+// 		if wordCount[word] == 5 {
+// 			continue
+// 		}
+//         wordCount[word]++
+//     }
+
+//     return wordCount
+// }
