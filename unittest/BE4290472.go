b83805575ec0987be5792be12e89635e42c35d7a
Mohammad Jamil Asfihani <mohammadjamilasfihani@gmail.com>
2023-05-05 08:49:29 +0700

    init commit

--- unittest/BE4290472.go
+++ unittest/BE4290472.go
@@ -1,20 +1,20 @@
 package unittest
 
-// import "math"
+import "math"
 
-// func HomeworkBE4290472(number int) bool {
-// 	if number > 50 {
-// 		return false
-// 	}
+func HomeworkBE4290472(number int) bool {
+	if number > 50 {
+		return false
+	}
 
-//     if number < 2 {
-//         return false
-//     }
+	if number < 2 {
+		return false
+	}
 
-//     for i := 2; i <= int(math.Sqrt(float64(number))); i++ {
-//         if number%i == 0 {
-//             return false
-//         }
-//     }
-//     return true
-// }
+	for i := 2; i <= int(math.Sqrt(float64(number))); i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+	return true
+}
